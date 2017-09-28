fee733670102ba3fa6684c746cdf0d8d5d24c4fd
ruthieirl <ruthfromjersey@gmail.com>
2017-09-28 16:21:03 -0400

    initial files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'KohjGHjRKFruI9nttDSBKR1oN',
+  consumer_secret: 'Dm8iVZO6a3N4b96ZPjzlKbCBVO1tiaZSOUvnfRzyE3udhsGjP2',
+  access_token_key: '913044507645575176-yPbOfEj3UHMJUROd9yXmUDMJCOJLNeq',
+  access_token_secret: 'xUzUm9XeXsp9KYuAd7I2b8NobDhFmbZTruvhPIzzLopsF',
+}
+
+module.exports = twitterKeys;
