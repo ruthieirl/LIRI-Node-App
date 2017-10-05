7b17d993fd2fb6ba56cc4171b53f4a00ce5e9dbf
ruthieirl <ruthfromjersey@gmail.com>
2017-10-05 10:20:22 -0400

    spotify block

--- liri.js
+++ liri.js
@@ -3,8 +3,9 @@ var keys = require("./keys.js");
 var fs = require("fs");
 var Twitter = require("twitter");
 var client = new Twitter("keys");
-var Spotify = require("spotify");
+var Spotify = require("node-spotify-api");
 var command = process.argv[2];
+var a = process.argv[3];
 var params = {
 	"screen_name" : "ruthiec_irl",
 	"count" : 20
@@ -23,12 +24,33 @@ function showTweets() {
 	})
 };
 
-function playSong() {
+function playSong(a) {
+	var spotify = new Spotify({
+		id: "29852749495a40fb8b8a5232f8b4c548",
+		secret: "b7fd7f906b784ae2a1c70a0cfc3a16d0",
+	});
 
+	spotify.search({
+		"type" : "track", 
+		"query" : a
+	}, function (error, data) {
+		if (error) {
+			console.log(error + "\n");
+		} else if (a === "") {
+			a = "The Sign";
+		} else {
+			console.log("\n****************************************\n");
+			console.log("Artist: " + data.tracks.item[i].album.artists[i].name);
+			console.log("Song: " + data.tracks.item[i].name);
+			console.log("Preview: " + data.tracks.item[i].preview_url);
+			console.log("Album: " + data.tracks.item[i].album.name);
+			console.log("\n****************************************\n");
+		}
+	})
 };
 
-function showMovie() {
-
+function showMovie(a) {
+	var myQuery = ""
 };
 
 function doSomething() {
